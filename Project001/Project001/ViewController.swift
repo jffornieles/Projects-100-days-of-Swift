bf5fcbea389599544c3bbde3c370512ddde0166c
jffornieles <jffornieles@gmail.com>
2019-04-05 13:40:47 +0200

    Obtenemos el nombre de las imagenes a mostrar

--- Project001/Project001/ViewController.swift
+++ Project001/Project001/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    var pictures = [String]()
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
-
-
+    
 }
 
