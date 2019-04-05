9997d513b3b898f70a56c4d03c1ef37ab2be3b6c
jffornieles <jffornieles@gmail.com>
2019-04-05 13:07:17 +0200

    Commit inicial con gitignore

---
+++ Project001/Project001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project001
+//
+//  Created by Jose Francisco Fornieles on 05/04/2019.
+//  Copyright © 2019 Jose Francisco Fornieles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
