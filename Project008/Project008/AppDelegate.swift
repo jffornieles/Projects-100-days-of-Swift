aa58459f78cc2367772ce93b8896b9bc2efbe451
jffornieles <jffornieles@gmail.com>
2019-06-04 11:49:52 +0200

    Project 8

---
+++ Project008/Project008/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Project008
+//
+//  Created by Jose Francisco Fornieles on 03/06/2019.
+//  Copyright © 2019 Jose Francisco Fornieles. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
